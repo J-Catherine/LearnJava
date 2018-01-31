29701ccf38a4749067fda8d3ce6e82923d2ba830
jwkr <865687826@qq.com>
2018-02-01 01:09:01 +0800

    p10. ye love nn forever~

--- EulerProject/Jan31st_10.java
+++ EulerProject/Jan31st_10.java
@@ -1,8 +1,23 @@
 public class Jan31st_10{
+    private static int M = 2000000;
     private static void p10(){
+        long sum = 0;
+        boolean[] m = new boolean[M];
+        for(int i = 0; i < M; i++){
+            m[i] = true;
+        }
+        for(int i=2; i < M; i++){
+            if(m[i]){
+                sum += i;
+                for(int j = 2 * i; j < M; j += i){
+                    m[j] = false;
+                }
+            }
+        }
+        System.out.println(sum);
 
     }
     public static void main(String[] args){
-        
+        p10();
     }
 }
