6d1f19b9eb3d4d94e7e5adef2719e34a31d13eb7
jwkr <865687826@qq.com>
2018-03-10 17:39:36 +0800

    亲和数要是不同的数才行哦！ 包含因数求和

--- ProjectEuler/Mar4th_21.java
+++ ProjectEuler/Mar4th_21.java
@@ -1,6 +1,32 @@
+
 public class Mar4th_21{
+    //find the sum of amicable pair
+    private static int FindSumFactor(int num){
+        int sumF = 1;  
+        int limit = (int)Math.sqrt(num);  
+        for(int i = 2 ; i <= limit; i++){  
+            if(num % i == 0)  
+                sumF += i + num / i;  
+        }  
+        return sumF;   
+    }
     private static void p21(){
+        int sum = 0;
+        int[] a = new int[10000];
+        a[1] = 1;
+        for(int i = 1; i < 10000; i++){
+            a[i] = FindSumFactor(i);
+            if(a[i] <= i){
+                if(a[a[i]] == i && a[i] != i){
+                    System.out.printf("%-5d and %-5d are an amicable pair.\n",a[i],i);
+                    sum += i;
+                    sum += a[i];
+                }
+            }
+        }
+        System.out.print(sum);
     }
     public static void main(String[] args){
+        p21();
     }
 }
