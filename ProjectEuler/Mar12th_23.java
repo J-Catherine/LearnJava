ba683cceedeaf2f9879488f0620b29226f61fbc2
jwkr <865687826@qq.com>
2018-03-22 21:21:08 +0800

    修正了23题，尽量不要用除法和开根号，用乘法替代他们

--- ProjectEuler/Mar12th_23.java
+++ ProjectEuler/Mar12th_23.java
@@ -1,43 +1,41 @@
 public class Mar12th_23{
     //find the sum of the number's proper divisors
+    static int x = 28123;
     private static int FindSumFactor(int num){
-        int sumF = 1;  
-        int limit = (int)Math.sqrt(num);  
-        for(int i = 2 ; i <= limit; i++){  
-            if(num % i == 0)  
-                sumF += i + num / i;  
-        }  
-        return sumF;   
+        int sumF = 1; 
+        int i = 0; 
+        for(i = 2 ; i * i < num; i++){  
+            if(num % i == 0){
+                sumF += i + num / i;
+            }  
+        }
+        if(i * i == num){
+            sumF += i;
+        }
+        return sumF;
     }
 
     private static void p23(){
         int[] a = new int[100000]; //abundant number
-        int count = 0;
-        int flag = 0;
-        for(int i = 1; i <= 28123; i++){
-            int temp = 0;
-            temp = FindSumFactor(i);
-            if(temp > i && temp != 5){
-                //System.out.printf("%d : %d\n", count, i);
-                if(flag == 0){
-                    System.out.printf("%d  %d\n",temp ,i);
-                    flag = 1;
-                }
+        int count = 0;//count the number of yingshu
+        for(int i = 1; i <= x; i++){
+            int temp = FindSumFactor(i);
+            if(temp > i){
                 a[count++] = i;
             }
         }
         //a[count] every value plus
-        int[] res = new int[28124];
-        for(int i = count - 1; i > 0; i--){
-            for(int j = 0; j < i; j++){
+        int[] res = new int[x+1];
+        for(int i = count - 1; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
                 int temp = a[i] + a[j];
-                if(temp <= 28123){
+                if(temp <= x){
                     res[temp] = 1;
                 }
             }
         }
         int sum = 0;
-        for(int i = 1; i <= 28123; i++){
+        for(int i = 1; i <= x; i++){
             if(res[i] == 0){
                 sum += i;
             }
