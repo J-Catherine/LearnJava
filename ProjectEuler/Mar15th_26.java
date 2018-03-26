43190c0b3570a4141dac30e65ed22f0a89d0d10a
jwkr <865687826@qq.com>
2018-03-26 20:25:13 +0800

    p26修正 一个数把质因子中的2和5除去后，这个数和原来数的倒数的循环节长度是相同的。对100求基数余，结果为1时就是循环节的长度

--- ProjectEuler/Mar15th_26.java
+++ ProjectEuler/Mar15th_26.java
@@ -2,8 +2,8 @@ public class Mar15th_26{
     private static void p26(){
         //first we need find prime factors of the number
         long[] n = new long[1001];
-        int count = 0;
-        for(int i = 1; i < 10; i++){
+        int count = 1;
+        for(int i = 1; i < 1000; i++){
             int temp = i;
             for(; temp % 2 == 0;){
                 temp /= 2;
@@ -11,41 +11,54 @@ public class Mar15th_26{
             for(; temp % 5 == 0;){
                 temp /= 5;
             }
-            if(temp != 1){
-                n[temp] = temp;
-                System.out.printf("numver is %d, value is %d\n",i,temp);
+            int x = 1;
+            for(int j = 1; j <= temp; j++){
+                x *= 10;
+                x %= temp;
+                if(x == 1){
+                    n[i] = j;
+                    break;
+                }
             }
         }
 
-        //System.out.println(count);
-        int max = 0;
-        int num = 0;
-        for(int i = 1; i < 10; i++){
-            long temp = 9;
-            for(; ;){
-                if(temp % n[i] == 0){
-                    n[i] = temp / n[i];
-                    for(int j = 0; ; j++){
-                        if(n[i] > 0){
-                            n[i] /= 10;
-                        }
-                        else if(n[i] == 0){
-                            if(j > max){
-                                max = j;
-                                num = i;
-                            }
-                            break;
-                        }
-                    }
-                    break;
-                }
-                else{
-                    temp = temp * 10 + 9;
-                }
+        int max = 1; 
+        for(int i = 1; i < 1000; i++){
+            if(n[i] > n[max]){
+                max = i;
             }
         }
-        System.out.printf("max is %d, the number is %d", max, num);
+        System.out.println(max);
     }
+
+    //     int max = 0;
+    //     int num = 0;
+    //     for(int i = 1; i < 10; i++){
+    //         long temp = 9;
+    //         for(; ;){
+    //             if(temp % n[i] == 0){
+    //                 n[i] = temp / n[i];
+    //                 for(int j = 0; ; j++){
+    //                     if(n[i] > 0){
+    //                         n[i] /= 10;
+    //                     }
+    //                     else if(n[i] == 0){
+    //                         if(j > max){
+    //                             max = j;
+    //                             num = i;
+    //                         }
+    //                         break;
+    //                     }
+    //                 }
+    //                 break;
+    //             }
+    //             else{
+    //                 temp = temp * 10 + 9;
+    //             }
+    //         }
+    //     }
+    //     System.out.printf("max is %d, the number is %d", max, num);
+    // }
     public static void main(String[] args){
         p26();
     }
